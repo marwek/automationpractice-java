43fab9d9412b597265b384b0166dfade8241fc6a
Mruva <mruva@live.com>
2019-05-21 14:02:48 +0200

    fix missing import

--- src/main/java/com/automationpractice/pageobjects/CreateAccount.java
+++ src/main/java/com/automationpractice/pageobjects/CreateAccount.java
@@ -2,6 +2,7 @@ package com.automationpractice.pageobjects;
 
 import java.util.List;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
